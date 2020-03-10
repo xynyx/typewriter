83388c6f4b24e767ee23ce4916ebe1d2ba78a0f2
Matt Taylor <matt.taylr22@gmail.com>
2020-03-10 14:34:01 +0000

    typewriting a given string

---
+++ typewriter.js
@@ -0,0 +1,8 @@
+const sentence  = "hello there from lighthouse labs";
+
+let timer = 0;
+for (const char of sentence) {
+  setTimeout( () =>  process.stdout.write(char), timer += 50);
+};
+
+setTimeout( () => process.stdout.write("\n"), timer);
